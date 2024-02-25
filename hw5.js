d5d0f43f4e7d92b4f06be55c6228fcc75c03a7fb
mjones120 <157424025+mjones120@users.noreply.github.com>
2024-02-25 18:32:06 -0500

    Add files via upload

---
+++ hw5.js
@@ -0,0 +1,21 @@
+// 1. Create a variable via a ternary operator that assigns a string based on an expression.
+//let isRainy = true? "Yes" : "No";
+//2. Create an object and loop through its properties, console logging each properties’ value
+// let student = {
+//  firstName: "Morgan",
+//   lastName: "Jones",
+//   age: 20,
+//   major: "Computer Science",}
+// for (let property in student) {
+//   console.log(property, student[property]);}
+//3. Create an array of numbers. Using the map function, create a second array of those numbers squared.
+// let numbersArray = [2,4,8,16,32,64,128];
+//let squaredNumbers = numbersArray.map(function(number){
+    //return number * number;
+//});
+//console.log(squaredNumbers);
+//4. What is the DOM? What is meant by the DOM tree?
+// DOM is the document object model that represents the webpages loaded into the browser. The DOM tree tells the browser how to structure the model. This model is made up of objects
+//5. What is a XSS attack? How do they work? How can you guard against them?
+// An XSS attack is when attacker adds malicious code into a webpage. The attacker finds a flaw in the code and then injects code into it to attack it. You can guard against them by being cautious with the content you put on an html. Additionally, you could validate input ands strip it of any malicious content
+
