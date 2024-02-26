c39b33b4c6009903d99e07a6a46baa49a98ee940
mjones120 <157424025+mjones120@users.noreply.github.com>
2024-02-25 21:53:36 -0500

    Update hw5.js

--- hw5.js
+++ hw5.js
@@ -17,5 +17,5 @@
 //4. What is the DOM? What is meant by the DOM tree?
 // DOM is the document object model that represents the webpages loaded into the browser. The DOM tree tells the browser how to structure the model. This model is made up of objects
 //5. What is a XSS attack? How do they work? How can you guard against them?
-// An XSS attack is when attacker adds malicious code into a webpage. The attacker finds a flaw in the code and then injects code into it to attack it. You can guard against them by being cautious with the content you put on an html. Additionally, you could validate input ands strip it of any malicious content
+// An XSS attack is when an attacker puts malicious code into a website that often has many different content creators. These attacks allow the attacker to have access to the DOM, cookies, and session tokens. This could lead the attacker to use user data to make purchases or post defaming comments. You can guard against them by being cautious with the content you put on an html. Additionally, you could validate input ands strip it of any malicious content
 
